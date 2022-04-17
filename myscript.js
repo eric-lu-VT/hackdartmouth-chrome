c63831e95f5e0b761b0c80d83505e799876e6c78
11Some1 <68611574+11Some1@users.noreply.github.com>
2022-04-17 01:34:38 -0400

    send website data to google api

--- myscript.js
+++ myscript.js
@@ -1,5 +1,11 @@
 setTimeout(() => {
 	res = document.getElementsByClassName("css-1dbjc4n r-1s2bzr4")[0].innerText;
-  // class="css-901oao css-16my406 r-poiln3 r-bcqeeo r-qvutc0"
-  console.log('hi' + res);
+  console.log(res);
+  /*
+  chrome.runtime.sendMessage({
+    data: res
+  }, function (response) {
+    console.dir(response);
+  });
+  */
 }, 2000);
--- twittertab.js
+++ twittertab.js
@@ -3,11 +3,11 @@ const TWITTER_URL = "https://twitter.com/"
 const getOpenTwitter = async () => {
   const allTabs = await chrome.tabs.query({})
   const currTab = (await chrome.tabs.query({ currentWindow: true, active: true }))[0]
-  console.log(currTab)
+  // console.log(currTab)
 
   const twitterOpen = allTabs.some((tab) => tab.url.includes(TWITTER_URL))
   const twitterCurr = currTab.url.includes(TWITTER_URL)
-  const twitterCurrTab = twitterCurr? currTab: null
+  const twitterCurrTab = twitterCurr ? currTab: null
   return { twitterOpen, twitterCurr, twitterCurrTab }
 }
 
