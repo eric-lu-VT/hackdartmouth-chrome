68505c9519b60fcd79f906097104795b994de647
11Some1 <68611574+11Some1@users.noreply.github.com>
2022-04-17 00:26:13 -0400

    connect to google api

--- myscript.js
+++ myscript.js
@@ -1,3 +1,5 @@
 setTimeout(() => {
-	console.log(document.getElementsByClassName("css-1dbjc4n r-1s2bzr4")[0].innerText);
-}, 1000);
+	res = document.getElementsByClassName("css-1dbjc4n r-1s2bzr4")[0].innerText;
+  // class="css-901oao css-16my406 r-poiln3 r-bcqeeo r-qvutc0"
+  console.log('hi' + res);
+}, 2000);
