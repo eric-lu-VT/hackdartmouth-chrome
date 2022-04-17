af8714decd52f138610b552c78f9c827c2e60f5f
rishavchakra <rishavchakra@gmail.com>
2022-04-16 23:17:05 -0400

    Starting front-back connection

--- popup.js
+++ popup.js
@@ -1,24 +1,50 @@
-// Initialize butotn with users's prefered color
-let changeColor = document.getElementById("changeColor");
+import { getOpenTwitter } from './twittertab'
+import { getSuggestions, getAnalysis } from './suggestions'
+
+// Initialize button with users's prefered color
+/* let changeColor = document.getElementById("changeColor");
 
 chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
+}); */
 
-// When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: setPageBackgroundColor,
-  });
-});
-
-// The body of this function will be execuetd as a content script inside the
+// The body of this function will be executed as a content script inside the
 // current page
-function setPageBackgroundColor() {
+/* function setPageBackgroundColor() {
   chrome.storage.sync.get("color", ({ color }) => {
     document.body.style.backgroundColor = color;
   });
+} */
+const retrieveTwitterText = () => {
+  return [
+    "This is the ugliest, nastiest, most appallingly terrible thing I have ever seen",
+    "I love rainbows! Roses are so pretty! Everything is great about today :)",
+    "Someday everybody you love will die.",
+    "I hate politics. It's so divisive and depressing to deal with. I wish it would just end."
+  ]
 }
+
+function setEmotionData() {
+  console.log('Setting the emotion data')
+  const { twitterOpen, twitterCurrent, twitCurrTab } = getOpenTwitter()
+  console.log('Setting the emotion data')
+
+  if (!twitterCurrent) {
+    // Twitter's not open, dummy!
+    console.log("Twitter isn't open right now!")
+    return
+  }
+
+  // get the text from the twitter page
+  const twitterText = retrieveTwitterText()
+  // score the retrieved text
+  const average = twitterText.reduce((prev, cur) => prev + cur, 0) / twitterText.length
+  const magnitude = twitterText.reduce((prev, cur) => prev + Math.abs(cur), 0) / twitterText.length
+  // Generate some text to display
+  // display the text and the bars
+
+}
+
+// getOpenTabs({})
+setEmotionData()
+
---
+++ twittertab.js
@@ -0,0 +1,14 @@
+const TWITTER_URL = "https://twitter.com/"
+
+const getOpenTwitter = async () => {
+  const allTabs = await chrome.tabs.query({})
+  const currTab = await chrome.tabs.getCurrent()
+
+  const twitterOpen = allTabs.some((tab) => tab.url.includes(TWITTER_URL))
+  const twitterCurr = currTab.includes(TWITTER_URL)
+  const twitterCurrTab = twitterCurr? currTab: None
+  return { twitterOpen, twitterCurr, twitterCurrTab }
+}
+
+export { getOpenTwitter }
+module.exports.getOpenTwitter = getOpenTwitter
