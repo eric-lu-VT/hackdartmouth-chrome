edbc13fe8371909ca9411f72e21137663092483b
11Some1 <68611574+11Some1@users.noreply.github.com>
2022-04-17 04:22:58 -0400

    add brackets

--- myscript.js
+++ myscript.js
@@ -3,9 +3,10 @@ chrome.runtime.onMessage.addListener(
     console.log(sender.tab ?
                 "from a content script:" + sender.tab.url :
                 "from the extension");
-    if (request.greeting === "hello")
+    if (request.greeting === "hello") {
       res = document.getElementsByClassName("css-1dbjc4n r-1s2bzr4")[0].innerText;
       console.log(res);
       sendResponse({message: res});
+    }
   }
 );
